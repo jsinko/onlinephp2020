7d9ffd3053f31c9064be3a295f330cd95bc9f2e5
Your Name <you@example.com>
2020-01-22 15:16:36 +0100

    model mjesto i zupanija

---
+++ app/Zupanija.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Zupanija extends Model
+{
+    public function mjestas() {
+        return $this->hasMany('App\Mjesto');
+    }
+}
